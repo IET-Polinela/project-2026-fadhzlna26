e20f2baec9d44bfa059dd52dd577ca1a078c6c48
Fadhila Azliana <fadhilaazliana@gmail.com>
2026-04-05 03:34:19 +0700

    Lab Session 03

--- SmartCity/iet_2026_24782009/iet_2026_24782009/settings.py
+++ SmartCity/iet_2026_24782009/iet_2026_24782009/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'iet_2026_24782009.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'smartcity_db',
+        'USER': 'postgres',
+        'PASSWORD': 'lilicantik',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- SmartCity/iet_2026_24782009/main_app/views.py
+++ SmartCity/iet_2026_24782009/main_app/views.py
@@ -1,4 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Report
+from .forms import ReportForm
 
+
+# HOME
 def home(request):
     return render(request, 'dhila_app/home.html')
+
+
+# READ (halaman daftar laporan)
+def list_report(request):
+    reports = Report.objects.all()
+    return render(request, 'dhila_app/list_report.html', {'reports': reports})
+
+
+# CREATE
+def add_report(request):
+    if request.method == "POST":
+        form = ReportForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('list_report')
+    else:
+        form = ReportForm()
+
+    return render(request, 'dhila_app/add_report.html', {'form': form})
+
+
+# UPDATE
+def update_report(request, id):
+    report = Report.objects.get(id=id)
+
+    if request.method == "POST":
+        form = ReportForm(request.POST, instance=report)
+        if form.is_valid():
+            form.save()
+            return redirect('list_report')
+    else:
+        form = ReportForm(instance=report)
+
+    return render(request, 'dhila_app/update_report.html', {'form': form})
+
+
+# DELETE
+def delete_report(request, id):
+    report = Report.objects.get(id=id)
+    report.delete()
+    return redirect('list_report')
