2d2a7cc84fc9b440646f91888a0e08f6dbe5054e
Fadhila Azliana <fadhilaazliana@gmail.com>
2026-04-29 14:29:46 +0700

    Lab 7 - No.3 Implementasi Backend

--- SmartCity/iet_2026_24782009/dashboard_24782009/views.py
+++ SmartCity/iet_2026_24782009/dashboard_24782009/views.py
@@ -1,4 +1,21 @@
 from django.views.generic import TemplateView
+from django.http import JsonResponse
+from main_app.models import Report
+from django.db.models import Count
+
 
 class DashboardView(TemplateView):
     template_name = 'dashboard/dashboard.html'
+
+
+def dashboard_data(request):
+    # Data status
+    status_data = Report.objects.values('status').annotate(total=Count('id'))
+
+    # Data kategori
+    category_data = Report.objects.values('category').annotate(total=Count('id'))
+
+    return JsonResponse({
+        'status': list(status_data),
+        'category': list(category_data),
+    })
