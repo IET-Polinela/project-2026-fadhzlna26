c8b1e2c81fdeb7e950a3f4631aaa88a982763f84
Fadhila Azliana <fadhilaazliana@gmail.com>
2026-05-20 13:54:18 +0700

    LabSession_10_ 2. Routing Token (Login & Refresh)

--- SmartCity/iet_2026_24782009/iet_2026_24782009/urls.py
+++ SmartCity/iet_2026_24782009/iet_2026_24782009/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 def welcome(request):
     return HttpResponse("<h1> Selamat Datang Fadhila Azliana")
@@ -14,4 +15,9 @@ urlpatterns = [
     path('', include('usermanagement_24782009.urls')),
     path('dashboard/', include('dashboard_24782009.urls')),
     path('api/', include('main_app.api_urls')),
+]
+
+urlpatterns += [
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
