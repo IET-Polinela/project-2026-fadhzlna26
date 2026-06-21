837a11c6f9789959380ecd5c525bb9a258ebc3cb
Fadhila Azliana <fadhilaazliana@gmail.com>
2026-06-22 04:50:32 +0700

    Menyembunyikan endpoint register dari dokumentasi OpenAPI

--- SmartCity/server_smartcity/usermanagement_24782009/api_views.py
+++ SmartCity/server_smartcity/usermanagement_24782009/api_views.py
@@ -1,8 +1,9 @@
 from rest_framework import generics, permissions
 from .models import CustomUser
 from .serializers import RegisterSerializer
+from drf_spectacular.utils import extend_schema
 
-
+@extend_schema(exclude=True)
 class RegisterView(generics.CreateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = RegisterSerializer
